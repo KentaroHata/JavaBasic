bfbd74e7dbc780ef83fc1d6e009cb08287d3db27
KentaroHata <KentaroHata@users.noreply.github.com>
2019-02-05 10:06:45 +0900

    practice

---
+++ Practice/src/Practice8.java
@@ -0,0 +1,25 @@
+
+
+public class Practice8 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		java.util.Scanner scanner = new java.util.Scanner(System.in);
+		System.out.println("１～９までの数字を入力して下さい。");
+		int num = scanner.nextInt();
+
+		if(num==1) {System.out.println("おはようございます。");
+		}else if(num==2) {System.out.println("おはようございます。");
+		}else if(num==3) {System.out.println("おはようございます。");
+		}else if(num==4) {System.out.println("こんにちは。");
+		}else if(num==5) {System.out.println("こんにちは。");
+		}else if(num==6) {System.out.println("こんにちは。");
+		}else if(num==7) {System.out.println("こんばんわ。");
+		}else if(num==8) {System.out.println("こんばんわ。");
+		}else if(num==9) {System.out.println("こんばんわ。");
+
+
+
+	}
+	}
+}
