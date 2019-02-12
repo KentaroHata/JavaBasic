ffdbfe3ec85ca4172857206190c2e8d4f3e4e5f4
KentaroHata <KentaroHata@users.noreply.github.com>
2019-02-13 01:51:14 +0900

    practice

--- Practice/src/practice05/PTra05_04.java
+++ Practice/src/practice05/PTra05_04.java
@@ -23,9 +23,14 @@ public class PTra05_04 {
 		 */
 
 		// ①
+		boolean winFlg = true;
+		while (winFlg) {
 		System.out.println("0～6の整数を入力してください");
 
+
 		String line = scanner.nextLine();
+
+		winFlg = false;
 
 		switch(line) {
 			case "0":
@@ -33,6 +38,26 @@ public class PTra05_04 {
 				break;
 			case "1":
 				System.out.println("月曜");
+				break;
+			case "2":
+				System.out.println("火曜");
+				break;
+			case "3":
+				System.out.println("水曜");
+				break;
+			case "4":
+				System.out.println("木曜");
+				break;
+			case "5":
+				System.out.println("金曜");
+				break;
+			case "6":
+				System.out.println("土曜");
+				break;
+			default:
+				winFlg = true;
 		}
+		}
+
 	}
 }
